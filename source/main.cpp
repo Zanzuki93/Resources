db0e44d81e75d37f40e225e7fc14765a733bc3ae
Zanzuki93 <chazbonner@yahoo.com>
2016-07-19 14:32:36 -0500

    getting stuff in our folders

--- source/main.cpp
+++ source/main.cpp
@@ -1,10 +1,36 @@
+#if defined(_WIN32) || (WIN64)
+
+#include "SDL.h"
+
+#endif
+
+#if defined(__APPLE__)
 
 #include "SDL2/SDL.h"
 
-#include <stdio.h>
+#endif
 
+using namespace std;
+
+
+#include <stdio.h>
+#include <iostream>
 int main(int argc, char* argv[])
 {
+#if defined(_WIN32) || (WIN64) //this code is used to make sure this window that uses
+							  //SDL runs on Windows properly
+ cout << "Running on Windows :) "<<endl;
+
+#endif
+
+#if defined(__APPLE__)//this code is used to make sure this window that uses
+	  	  	  	  	  //SDL runs on Apple properly
+
+cout << "Running on Apple :) " <<endl;
+
+#endif
+
+
 SDL_Window *window;
 
 SDL_Init(SDL_INIT_VIDEO);
