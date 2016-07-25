7a7aedc7e65be87332f4bd2e84b763649094c988
Zanzuki93 <chazbonner@yahoo.com>
2016-07-25 10:45:05 -0500

    SDL_libraries installed
    now to get them doing stuff and get it to work on windows

--- source/main.cpp
+++ source/main.cpp
@@ -8,6 +8,12 @@
 
 #include "SDL2/SDL.h"
 
+#include "SDL2_image/SDL_image.h"
+
+#include "SDL2_mixer/SDL_mixer.h"
+
+#include "SDL2_ttf/SDL_ttf.h"
+
 #endif
 
 using namespace std;
@@ -37,7 +43,11 @@ SDL_Window *window;
 SDL_Init(SDL_INIT_VIDEO);
 
 window = SDL_CreateWindow("SDL Test Window",SDL_WINDOWPOS_UNDEFINED,
-		SDL_WINDOWPOS_UNDEFINED,640,480,SDL_WINDOW_OPENGL);
+		SDL_WINDOWPOS_UNDEFINED,1024,768,SDL_WINDOW_OPENGL);
+
+//code for making the level appear
+SDL_Surface * background;
+background = IMG_Load("/images/placeholder.png");
 
 if(window == NULL)
 {
