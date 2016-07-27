715b6b3728cf2f19322233cc3d8b6f06dc1dfd11
Zanzuki93 <chazbonner@yahoo.com>
2016-07-27 10:51:55 -0500

    Got Most of the Stuff Done
    Need to keep going though :)

--- source/main.cpp
+++ source/main.cpp
@@ -30,14 +30,6 @@ using namespace std;
 #include <iostream>
 int main(int argc, char* argv[])
 {
-SDL_Renderer* r1;
-SDL_Rect Background;
-
-Background.x = 200;
-Background.y = 200;
-Background.h = 500;
-Background.w = 500;
-
 #if defined(_WIN32) || (WIN64) //this code is used to make sure this window that uses
 							  //SDL runs on Windows properly
  cout << "Running on Windows :) "<<endl;
@@ -45,7 +37,7 @@ Background.w = 500;
 
  string currentWorkingDirectory(getcwd(NULL, 0));
 
- string images_dir = currentWorkingDirectory + "\\Resources\\images";
+ string images_dir = currentWorkingDirectory + "\\Resources\\images\\";
 
 #endif
 
@@ -58,6 +50,10 @@ string currentWorkingDirectory(getcwd(NULL, 0));
  string images_dir = currentWorkingDirectory + "/Resources/images/";
 #endif
 
+ SDL_Renderer* r1;
+
+ bool inGame = true;
+ SDL_Event event;
 
 SDL_Window *window;
 //initialize SDL
@@ -68,36 +64,209 @@ window = SDL_CreateWindow("SDL Test Window",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPO
 //create a renderer
 r1 = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-SDL_Surface *surface = IMG_Load((images_dir + "placeholder.png").c_str());
+//create the player texture
+SDL_Rect Player;
+Player.x = 500;
+Player.y = 400;
+Player.h = 10;
+Player.w = 10;
 
-//create a texture
+const int Player_Vel = 5;
 
-SDL_Texture * t1 = IMG_LoadTexture(r1, "placeholder.png");
+int PlayerVelX = 0;
+int PlayerVelY = 0;
 
-//Creates a texture using the surface
-t1 = SDL_CreateTextureFromSurface(r1,surface);
+SDL_Texture * t2 = IMG_LoadTexture(r1,(images_dir+"TestImage.png").c_str());
 
-SDL_FreeSurface(surface);
+//create the background texture
+SDL_Rect Background;
+SDL_Texture * t1 = IMG_LoadTexture(r1, (images_dir+"LevelDesignWIP.png").c_str());
+
+Background.x = 0;
+Background.y = -768;
+int w,h;
+SDL_QueryTexture(t1,NULL,NULL,&w,&h);
+Background.h = h;
+Background.w = w;
+
+//Setting Up the Walls
+/*SDL_Rect Wall;
+Wall.x = 975;
+Wall.y = 0;
+Wall.w = 2049;
+Wall.h = 1536;
+//SDL_Texture * w1 = IMG_LoadTexture(r1, (images_dir+"WallTexturePlaceholder.png").c_str());
+SDL_Rect Wall2;
+Wall2.x = 0;
+Wall2.y = -768;
+Wall2.w = 40;
+Wall2.h = 1536;
+//SDL_Texture * w2 = IMG_LoadTexture(r1, (images_dir+"WallTexturePlaceholder.png").c_str());
+SDL_Rect Wall3;
+Wall3.x = 40;
+Wall3.y = 0;
+Wall3.w = 2049;
+Wall3.h = 40;
+//SDL_Texture * w3 = IMG_LoadTexture(r1, (images_dir+"WallTexturePlaceholder.png").c_str());
+SDL_Rect Wall4;
+Wall4.x = 40;
+Wall4.y = 728;
+Wall4.w = 1025;
+Wall4.h = 40;
+//SDL_Texture * w4 = IMG_LoadTexture(r1, (images_dir+"WallTexturePlaceholder.png").c_str());
+*/
+//Error Messages For Texture File Paths
 if (t1 == NULL)
 {
-	cout << "Error Loading background :(" << endl;
-
+	//used to check to see if the background image is working
+	cout << "Error Loading Background :(" << endl;
 }
+if(t2 == NULL)
+{
+	cout <<"Error Loading Player :("<<endl;
+}
+
 if(window == NULL)
 {
 	printf("\n Couldn't create window :( ",SDL_GetError());
 	return 1;
 }
+while(inGame)
+{
+	//Input for Game//
+	while(SDL_PollEvent(&event)!= 0)
+	{
+	if(event.type == SDL_QUIT)
+	 {
+	 inGame = false;
 
+	 }
+	else
+	 {
+		  if(event.type == SDL_KEYDOWN && event.key.repeat == 0)
+		  {
+
+			  switch(event.key.keysym.sym)
+			  {
+				 case SDLK_ESCAPE:
+				 inGame = false;
+				 break;
+				 case SDLK_w:
+				 PlayerVelY -= Player_Vel;
+				 break;
+				 case SDLK_s:
+				 PlayerVelY += Player_Vel;
+				 break;
+				 case SDLK_a:
+				 PlayerVelX -= Player_Vel;
+				 break;
+				 case SDLK_d:
+				 PlayerVelX += Player_Vel;
+				 break;
+			  }
+		  }
+		  else if(event.type == SDL_KEYUP && event.key.repeat == 0)
+		  {
+			  switch(event.key.keysym.sym)
+			  {
+				 case SDLK_w:
+				 PlayerVelY += Player_Vel;
+				 break;
+				 case SDLK_s:
+				 PlayerVelY -= Player_Vel;
+				 break;
+				 case SDLK_a:
+				 PlayerVelX += Player_Vel;
+				 break;
+				 case SDLK_d:
+				 PlayerVelX -= Player_Vel;
+				 break;
+			}
+		 }
+	  }
+	}
+	//Input End//
+
+	//Update Player//
+	//Adjusting the screen Horizontally
+	Player.x += PlayerVelX;
+	Player.y += PlayerVelY;
+	if(Player.x > 1024 - (Player.w * 2))
+		{
+			Player.x = 1024 - (Player.w*2);
+			Background.x -= PlayerVelX;
+
+			/*Wall.x -= PlayerVelX;
+			Wall2.x -= PlayerVelX;
+			Wall3.x -= PlayerVelX;
+			Wall4.x -= PlayerVelX;*/
+		}
+	if(Player.x < 0 + (Player.w * 2))
+			{
+				Player.x = 0 + (Player.w*2);
+				Background.x -= PlayerVelX;
+
+				/*Wall.x -= PlayerVelX;
+				Wall2.x -= PlayerVelX;
+				Wall3.x -= PlayerVelX;
+				Wall4.x -= PlayerVelX;*/
+			}
+	//Checking For collision with walls and the player Left and Right
+		/*if(SDL_HasIntersection(&Player,&Wall) ||SDL_HasIntersection(&Player,&Wall2) ||
+				SDL_HasIntersection(&Player,&Wall3) || SDL_HasIntersection(&Player,&Wall4))*/
+	//Adjusting the screen Vertically
+	//Player.y += PlayerVelY;
+	if(Player.y > 768 - (Player.h * 2))
+			{
+				Player.y = 768 - (Player.h*2);
+				Background.y -= PlayerVelY;
+
+				/*Wall.y -= PlayerVelY;
+				Wall2.y -= PlayerVelY;
+				Wall3.y -= PlayerVelY;
+				Wall4.y -= PlayerVelY;*/
+			}
+		if(Player.y < 0 + (Player.h * 2))
+				{
+					Player.y = 0 + (Player.h*2);
+					Background.y -= PlayerVelY;
+
+					/*Wall.y -= PlayerVelY;
+					Wall2.y -= PlayerVelY;
+					Wall3.y -= PlayerVelY;
+					Wall4.y -= PlayerVelY;*/
+				}
+
+	//Checking For collision with walls Up and Down
+	/*if(SDL_HasIntersection(&Player,&Wall) ||SDL_HasIntersection(&Player,&Wall2) ||
+				SDL_HasIntersection(&Player,&Wall3) || SDL_HasIntersection(&Player,&Wall4))*/
+
+
+
+	//Update Player End//
+
+//SDL Drawing Process//
 // clear buffer and draw agents and walls.
+SDL_RenderClear(r1);
+//render a copy of the texture
 SDL_RenderCopy(r1, t1, NULL, &Background);
+//render what is presently in the buffer
+SDL_RenderCopy(r1,t2,NULL,&Player);
+
+//Wall Texture Code
+/*SDL_RenderCopy(r1,w1,NULL,&Wall);
+
+SDL_RenderCopy(r1,w2,NULL,&Wall2);
+
+SDL_RenderCopy(r1,w3,NULL,&Wall3);
+
+SDL_RenderCopy(r1,w4,NULL,&Wall4);*/
 
 SDL_RenderPresent(r1);
-
-SDL_Delay(3000);
-
-SDL_RenderClear(r1);
-
+//SDL Drawing Process End//
+SDL_Delay(16);
+}
+//End of Game Loop//
 SDL_DestroyWindow(window);
 
 SDL_Quit();
